285515122e993ea059961bad12791d1eb7f5642c
hernan gomez <hernan09.gz@gmail.com>
2019-08-23 20:55:08 -0300

    email

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
 const express = require('express');
-const cors = require('cors');
+
 const bodyParser = require('body-parser');
 const app = express();
 const nodemailer = require('nodemailer');
 
-let port =process.env.PORT || 3000
+let port =process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader('Content-Type', 'application/json')
+
+  next()
+})
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.post('/sendEmail', (req, res) => {
 
- function sender(formulario){
+app.post('/mailer',(req,res)=>{
+  let transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+      user: 'hernag_09@hotmail.com',
+      pass: 'Raptor09'
+    },
 
-           //via del email
-    let transport = nodemailer.createTransport({
-        service:'Gmail',
-        auth:{ 
-         user: 'hernag_09@hotmail.com',
-         pass: 'Raptor09'},
-
-        tls: {
-            rejectUnauthorized: false
-          }
-  
-    })
-    //opciones del email
-    let mailoptions = {
-        from: 'hernan',
-        to: 'hernag_09@hotmail.com',
-        subject: 'Hernan Gomez',
-        text: 'mail :' + req.body.email +' Mensaje :'+req.body.text,
-        html: '<ul><li>' + req.body.email + '</li><li>' + req.body.text + '</li></ul>'
-    
-      }
-       //se envia el mail
-      transporter.sendMail(mailoptions, (error, info) => {
-        if (error) console.log(`${error}`)
-        else {
-
-          console.log(info)
-         
-        }
-      })
-    
+    tls: {
+      rejectUnauthorized: false
     }
-   
-  
+
+  })
+
+  let mailoptions = {
+    from: 'hernan',
+    to: 'hgomez@cdt.com.ar',
+    subject: 'Hernan Gomez',
+    text: 'mail' + req.body.email +'texto : '+ req.body.texto,
+    html: '<ul><li style="list-style:none">' + req.body.email + '</li></ul>'+'<br>'+'<p>'+req.body.texto+'</p>'
+
+  }
+
+  transporter.sendMail(mailoptions, (error, info) => {
+    if (error) console.log(`${error}`)
+    else {
+      console.log(info)
+     res.send({mensaje:'mesaje success'})
+    }
+  },err=>{
+    console.log(err)
+  })
 })
 
 
 
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+ if(err) console.log(err)
 
 console.log(`http://localhost:${port}`)
 });
